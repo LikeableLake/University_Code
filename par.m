a745f21c5fc3a5f01e409ab97b7e051426b31828
Antonio <Antonio@DESKTOP-BTN7U5E>
2026-06-10 22:18:45 +0200

    parallelo a 4

--- par.m
+++ par.m
@@ -1,10 +1,12 @@
-function [eq] = par(A,B,C)
+function [eq] = par(A,B,C,D)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 if nargin==2
     eq=1/((1/A)+(1/B));
 elseif nargin==3
     eq=1/((1/A)+(1/B)+(1/C));
+elseif nargin==4
+    eq=1/((1/A)+(1/B)+(1/C)+(1/D));
 end
 
 
