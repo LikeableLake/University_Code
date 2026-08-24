9e8069af00aa2692b70866a94c31cb6123a18050
Antonio <Antonio@LAPTOP-4OLEFT8I>
2026-08-25 00:02:48 +0200

    co2

--- propulsione/cea.m
+++ propulsione/cea.m
@@ -1,6 +1,6 @@
-clc; close all; clear;
+clc; clear; close all
 
-filename = 'o2-ch4.txt';
+filename = 'o2-ch4-50atm.txt';
 
 % Leggi il file
 txt = fileread(filename);
@@ -10,15 +10,19 @@ OF_cell = regexp(txt, 'O/F=\s*([-+]?\d*\.?\d+)', 'tokens');
 
 % Trova T
 T_cell = regexp(txt, 'T,\s*K\s+([-+]?\d*\.?\d+)', 'tokens');
+CO2_cell = regexp(txt, '*CO2\s+([-+]?\d*\.?\d+)', 'tokens');
 
 % Converti le stringhe in numeri
 OF = cellfun(@(x) str2double(x{1}), OF_cell);
 T  = cellfun(@(x) str2double(x{1}), T_cell);
-
+CO2 = cellfun(@(x) str2double(x{1}), CO2_cell);
 % Crea tabella
-risultati = table(OF(:), T(:), ...
-    'VariableNames', {'OF', 'T_K'});
+risultati = table(OF(:), T(:), CO2(:), 'VariableNames', {'OF', 'T_K', 'CO2'});
 
 disp(risultati);
-plot(risultati, "OF", "T_K");
+yyaxis left; plot (risultati, "OF", "T_K"); hold on; 
+yyaxis right;
+plot (risultati, "OF", "CO2")
+
+
 max(risultati.T_K)
